71946f901e409fd1c0eed5df2f56074905db9914
Rishab Nayyar <82491413+rishabhacking@users.noreply.github.com>
2021-05-15 00:07:25 +0530

    Update and rename simpleportscanner.py to simpleportscanner-raw_input.py

--- simpleportscanner-raw_input.py
+++ simpleportscanner-raw_input.py
@@ -5,8 +5,8 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.setdefaulttimeout(2)
 
-host = raw_input("Enter the IP : ")
-port = int(raw_input("Enter the port :"))
+host = raw_input("[*] Enter the IP : ")
+port = int(raw_input("[*] Enter the port : "))
 
 def port_scanner(port):
 	if sock.connect_ex((host,port)):
